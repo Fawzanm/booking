c45b6dcc2a2add1e898555f2596fd53b3e8807c2
Mohamed Fawzan <fawzanm@gmail.com>
2019-06-12 10:31:22 +0400

    Added role model

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 10)->create();
+
+
+        App\User::create([
+            'email' => 'fawzanm@gmail.com',
+            'name' => 'Mohamed Fawzan',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm',
+
+        ]);
+
+
+    }
+}
