28dea5a61e492964c854c66ea03622661abe4fe2
Mohamed Fawzan <fawzanm@gmail.com>
2019-06-12 11:14:30 +0400

    Updated rooms, bookings migrations

---
+++ database/migrations/2019_06_12_071121_create_bookings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('room_id');
+            $table->unsignedBigInteger('user_id');
+            $table->date('from');
+            $table->date('to');
+            $table->integer('capacity');
+            $table->string('comments', 1023);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
